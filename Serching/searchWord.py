499098189a4c06a21143c0995df4d04b6c7fa5ba
TaiyouDong <3140104431@zju.edu.cn>
2017-06-14 22:19:00 +0800

    实现了短语查询

--- Serching/searchWord.py
+++ Serching/searchWord.py
@@ -13,6 +13,8 @@ def searchOneWord(index,word):
 
 #将所有word的结果取并，即所有包含这些word的文档
 def searchWords(index,words):
+    if len(words) == 0:
+        return []
     docQueue = queue.Queue()
     #print(wordset)
     for word in words:
@@ -25,3 +27,48 @@ def searchWords(index,words):
 
     list = docQueue.get()
     return list
+
+def searchPhrase(index,words,inputList):
+    if len(words) == 0:
+        return []
+    docQueue = queue.Queue()
+    for word in words:
+        docQueue.put(searchOneWord(index,word))
+
+    while docQueue.qsize() > 1:
+        list1 = docQueue.get()
+        list2 = docQueue.get()
+        docQueue.put(operateDocList.andTwoList(list1, list2))
+    doclist = docQueue.get()
+
+    resultList = {}
+
+    if len(inputList) == 1:
+        for doc in doclist:
+            resultList[doc] = index[inputList[0]][str(doc)]
+        return resultList
+
+    # print(doclist)
+    for docid in doclist:
+        docid = str(docid)
+        locList = []
+        x = index[inputList[0]][docid]
+        for loc in index[inputList[0]][docid]:
+            # print(index[inputList[0]][docid])
+            floc = loc
+            n = len(inputList)
+            hasFind = True
+            for word in inputList[1:n]:
+                floc += 1
+                try:
+                    # print(index[word][docid])
+                    index[word][docid].index(floc)
+                except:
+                    hasFind = False
+                    break
+            if hasFind:
+                locList.append(loc)
+        if len(locList) > 0:
+            resultList[docid] = locList
+    return resultList
+
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from InvertedIndex import establishIndex
 from LanguageAnalysis import stemming
 from Serching import searchWord
 from scoreQuery import sortDoc
-
 import tools
 import os
 
 directname = 'Reuters'
 
-#establishIndex.createIndex(directname)
+# print("establishing the index...")
+# establishIndex.createIndex(directname)
 
 print("getting word list...")
 wordList = getIndex.getWordList()
@@ -22,29 +22,58 @@ path = tools.projectpath + directname
 files = os.listdir(path)
 fileNum = len(files)
 
+loop = True
 print("=================Searching System=================")
-print("Input the query statement:")
-statement = input()
 
-while statement is not "EXIT":
-    #对输入的字符进行词干还原
-    inputWords = stemming.lemmatize_sentence(statement)
-    print(inputWords)
-    #去除input中重复的元素
-    wordset = set(inputWords)
-    docList = searchWord.searchWords(index,wordset)
-    # print(docList)
-    sortedDocList = sortDoc.TopKScore(20,index,fileNum,wordset,docList)
-    s2 = sortDoc.sortScoreDocList(index,fileNum,wordset,docList)
-    print(sortedDocList)
-    print(s2)
+while loop:
+    print("searching operation: ")
+    print("[1] Overall Search  [2]TOP K Search [3]bool search [4]Phrase Search [5]exit")
+    print("your choice(int):")
+    choice = int(input())
 
+    if choice >= 1 and choice <= 5:
+        print("input the query statement:")
+        statement = input()
+        if statement == "EXIT":
+            break
+        # 对输入的字符进行词干还原
+        inputWords = stemming.lemmatize_sentence(statement)
+        print(inputWords)
+        # 去除input中重复的元素
+        wordset = set(inputWords)
 
+        #查询排序
+        if choice == 1 :
+            docList = searchWord.searchWords(index, wordset)
+            sortedDocList = sortDoc.sortScoreDocList(index,fileNum,wordset,docList)
+            for doc in sortedDocList:
+                print("doc ID: ", doc[1], " score: ", "%.4f" % doc[0])
 
+        #TOP K 查询
+        elif choice == 2:
+            docList = searchWord.searchWords(index, wordset)
+            sortedDocList = sortDoc.TopKScore(20, index, fileNum, wordset, docList)
+            for doc in sortedDocList:
+                print("doc ID: ", doc[1], " score: ", "%.3f" % doc[0])
 
-    print("Input the query statement:")
-    statement = input()
+        elif choice == 3:
+            pass
 
+        elif choice == 4:
+            phraseDocList = searchWord.searchPhrase(index,wordset,inputWords)
+            if len(phraseDocList) == 0:
+                print("Doesn't find \"",statement, '"')
+            else:
+                for key in phraseDocList.keys():
+                    print('docID: ',key, "   num: ",len(phraseDocList[key]))
+                    print('    location: ',phraseDocList[key])
+        elif choice == 5:
+            break
+    else:
+        print("Invalid choice! Please observe these choices carefully!")
+    print()
+
+print("ByeBye!")
 
 
 # establishVSM.createVSM(index,wordList,'test')
